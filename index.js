2e66ce75b097c6f362907a0a1729ec46a25235c2
unknown <BT_3W2_32@BKTECH.LOCAL>
2024-10-25 10:51:30 -0400

    initial commit

--- index.js
+++ index.js
@@ -3,17 +3,52 @@
 // variables representing two cards 
 let firstCard = 10
 let secondCard = 4
+let sum = firstCard + secondCard;
 
-//challenge #1
-//create a new variable and set it to the sum of the two cards 
+const cards = [];
+let hasBlackjack = false;
+let isAlive = false;
+let message = "Do you want to draw a new card? 🙂";
 
+const messageEl = document.getElementById("message-el");
+const sumEl = document.getElementById("sum-el");
+const cardsEl = document.getElementById("cards-el");
 
-//chalenge #2 
-// code the statements below based on the conditions shown in blackjack.png from the images folder
-// your output should work in the browser's console based on changing the values assigned to the cards
+// message = "Do you want to draw a new card? 🙂"
+// message = "Wohoo! You've got Blackjack! 🥳"
+// message = "You're out of the game! 😭"
 
-//starter code
-    message = "Do you want to draw a new card? 🙂"
-    message = "Wohoo! You've got Blackjack! 🥳"
-    message = "You're out of the game! 😭"
-console.log(message)
+const renderGame = () => {
+    cards.forEach((v) => {
+        cardsEl.textContent += v;
+    })
+
+    sum = cards.reduce((a, b) => a + b);
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum > 21) {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    } else {
+        message = "Wohoo! You've got Blackjack! 🥳";
+        hasBlackjack, isAlive = true;
+    }
+}
+
+const newCard = () => {
+    let card = getRandomCard();
+    cards.push(card);
+    renderGame();
+}
+
+const startGame = () => {
+    isAlive = true;
+    firstCard = getRandomCard();
+    secondCard = getRandomCard();
+    cards.push(firstCard, secondCard);
+    renderGame();
+}
+
+const getRandomCard = () => {
+    
+}
